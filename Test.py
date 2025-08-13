97c6f88e511e0e3dc4799c158749cccd5d4aea37
Ziaul Karim <ziaulkarim278@gmail.com>
2025-08-13 18:14:15 +0600

    Conditional statements, nested if-else, while loops in Python

--- Test.py
+++
@@ -1,9 +0,0 @@
-password = input("Enter a password: ")
-len = len(password)
-if len >= 8:
-  if "AI"  in password:
-    print("Strong Password")
-  else:
-    print("Weak Password")
-else:
-  print("Invalid Password")
